acd910e58c022a5551fe00ec63d5d5a9e946ba44
59parthiban39 <35764937+59parthiban39@users.noreply.github.com>
2018-03-12 16:07:55 +0530

    Update set64\12\03.C

--- set64\12\03.C
+++ set64\12\03.C
@@ -1,18 +1,28 @@
 #include <stdio.h>
  
+
 int main(void) 
 {
 	int num,even;
+	
 	printf("enter the number:");
 	scanf("%d",&num);
+	
 	if(num%2==0)
 	{
 		even=num-2;	
+		
 	}
 	else
+		
+		
 	{
 		even=num-1;
+		
 	}
+	
 	printf("\n%d",even);
+	
 	return 0;
 }
+
